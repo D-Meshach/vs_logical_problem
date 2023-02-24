8afc9bf7de371098dd5850086571b50a9a70aa3e
D-Meshach <meshachdaniel456@gmail.com>
2023-02-25 05:24:51 +0530

    Fibonacci series aded

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logical
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String i = "y";
+            while (i == "y") {
+                Console.WriteLine("Enter option \n 1)Fibonacci Series \n  \nEnter option here-");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+
+                    case 1: fibonacci_series.fibonacci(); break;
+                    default: Console.WriteLine("Enter a valid statement"); break;
+
+                }
+                Console.WriteLine("Do you wish to continue (y/n)?");
+                i= Convert.ToString(Console.ReadLine());
+
+
+            }
+
+            
+
+        }
+    }
+}
---
+++ fibonacci_series.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace Logical
+{
+    public class fibonacci_series
+    {
+
+        public static void fibonacci() {
+            int temp=0;
+            Console.WriteLine("Enter the count for number of iterations");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int[] ar = new int[2];
+            temp = 1;
+            ar[0] = 0;
+            ar[1] = 1;
+            Console.WriteLine("The Fibonacci series are");
+            Console.WriteLine(0);
+            for (int i =0; i < num-1; i++)
+            {
+                temp = ar[1] + ar[0];
+                ar[0] = ar[1];
+                ar[1] = temp;
+
+                Console.WriteLine(temp);
+               
+                
+
+            }
+
+        }
+
+    }
+}
